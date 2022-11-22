69c5be20e49c7c59bdfd04a19c18bfcd977a3c61
Shohzod-xv <xayrullayevshohzod2@gmail.com>
2022-11-22 19:14:01 +0500

    start adminka menu-dashboard

---
+++ src/components/slidebar/Slidebar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+      <div class="sidebar-brand">
+        <a href="index.html"> <span
+            class="logo-name">Adminka</span>
+        </a>
+      </div>
+      <ul class="sidebar-menu">
+        <li class="menu-header">Main</li>
+        <li class="dropdown active">
+          <a href="index.html" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+        </li>
+        <li class="dropdown">
+          <a href="index.html" class="nav-link"><i data-feather="monitor"></i><span>Users</span></a>
+        </li>
+      </ul>
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Slidebar"
+}
+</script>
+
+<style scoped>
+.logo-name{
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: #0ca605;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/css/app.min.css'
+import './assets/css/style.css'
+import './assets/css/components.css'
+import './assets/css/custom.css'
+import './assets/img/favicon.ico'
+
+createApp(App).mount('#app')
