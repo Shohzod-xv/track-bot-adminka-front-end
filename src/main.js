99b95b1dbbb1d6dcc232b0f80c2dbeb31ae3c431
Shohzod-xv <xayrullayevshohzod2@gmail.com>
2022-11-24 08:38:19 +0500

    js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import './assets/css/app.min.css'
 import './assets/css/style.css'
 import './assets/css/components.css'
-import './assets/css/custom.css'
 import './assets/img/favicon.ico'
 import router from "./router";
 createApp(App).use(router).mount('#app')
